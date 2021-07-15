f3332d78e926bc4bca2044224d82b6e20562f642
mayank1261 <71663622+mayank1261@users.noreply.github.com>
2021-07-15 15:56:30 +0530

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,29 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() {
+background("white");
+  if(keyIsDown(UP_ARROW)){
+  background("lavender");
+
+ }
+ if(keyIsDown(LEFT_ARROW)){
+   background("lightblue");
+ }
+if(keyIsDown(DOWN_ARROW)){
+  background("lightpink");
+}
+if(keyIsDown(RIGHT_ARROW)){
+  background("lightyellow")
+}
+}
+
+ 
+
+
+
+
+
